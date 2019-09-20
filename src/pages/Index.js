c1d904609cf938ed0290ec1910219e9fee171c66
KokiYamamoto <y.koki1023@gmail.com>
2019-09-20 23:43:16 +0900

    [Add]Indexページを作成

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import Paper from '@material-ui/core/Paper'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import "../css/Index.css"
+import Divider  from '@material-ui/core/Divider';
+import { Typography } from '@material-ui/core';
 
 function Index() {
     return(
-        <p>Index</p>
+        <div>
+            <Header/>
+            <div style={{position:"relative",marginTop:"2%",marginBottom:"5%"}}>
+                <div className = "IndexBlurBg">
+                    <div className = "IndexMainTextField">
+                        <Typography variant = "h2"> Welcome to Review Site! </Typography>
+                    </div>
+                </div>
+            </div>
+            <Divider/>
+            <div className="IndexContentBox">
+                <Paper className = "IndexContent">
+                    <Typography variant="body1">
+                        Latest Reviews
+                    </Typography>
+                </Paper>
+                <Divider orientation="vertical"/>
+                <Paper className = "IndexContent">
+                    <Typography variant="body1">
+                        Latest Reviews
+                    </Typography>
+                </Paper>
+                <Divider orientation="vertical"/>
+                <Paper className = "IndexContent">
+                    <Typography variant="body1">
+                        Latest Reviews
+                    </Typography>
+                </Paper>
+            </div>
+            <Divider/>
+            <Footer/>
+        </div>
     )
 }
 
