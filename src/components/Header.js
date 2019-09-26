e4649db15295435f172d6b006f0cf5134cdff710
KokiYamamoto <y.koki1023@gmail.com>
2019-09-26 14:15:09 +0900

    [Add]Login後Headerにユーザー名を表示

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,33 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
 import SearchBar from './SearchBar'
 
-function Header() {
+
+
+function Header(props) {
+
+    const handleLoggedIn = () => {
+        if (props.username !== ""){
+            return(
+                <Typography variant = "body1"> {props.username} </Typography>
+            )
+        }else{
+            return(
+                <Link 
+                    to = "/login"
+                    style = {{textDecoration:"none"}}
+                >
+                    <Button> Login </Button>
+                </Link>
+            )
+        }
+    }
+
     return(
         <div style = {{flexGrow:1}}>
             <AppBar position="relative" >
@@ -18,21 +38,15 @@ function Header() {
                         underline = "none"
                     >
                        
-                            <Typography 
-                                variant = "h6"
-                                style = {{color: "white"}}
-                            > 
-                                Review Site 
-                            </Typography>
-                        
+                        <Typography 
+                            variant = "h6"
+                            style = {{color: "white"}}
+                        > 
+                            Review Site 
+                        </Typography>
                     </Link>
                     <SearchBar/>
-                    <Link 
-                        to = "/login"
-                        style = {{textDecoration:"none"}}
-                    >
-                        <Button> Login </Button>
-                    </Link>
+                    {handleLoggedIn()}
                 </Toolbar>
             </AppBar>
             
@@ -40,4 +54,10 @@ function Header() {
     )
 }
 
-export default Header
+const mapStateToProps = state => {
+    return{
+        username: state.Login.username
+    }
+}
+
+export default connect(mapStateToProps)(Header)
